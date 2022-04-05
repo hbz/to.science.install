77ed5ea77f74146f934a6981e86423a60df85d82
Ingolf Kuss <kuss@hbz-nrw.de>
2022-04-05 16:00:57 +0200

    Anpassungen an copy-config.sh wg. SLES/toscience

--- copy-config.sh
+++ copy-config.sh
@@ -8,14 +8,14 @@ cp $ARCHIVE_HOME/conf/tomcat-users.xml $TOMCAT_CONF
 cp $ARCHIVE_HOME/conf/fedora-users.xml $ARCHIVE_HOME/fedora/server/config/
 cp $ARCHIVE_HOME/conf/fedora.fcfg $ARCHIVE_HOME/fedora/server/config/
 cp $ARCHIVE_HOME/conf/setenv.sh $TOMCAT_HOME/bin
-cp $ARCHIVE_HOME/conf/application.conf $ARCHIVE_HOME/regal-server/conf
+cp $ARCHIVE_HOME/conf/application.conf $ARCHIVE_HOME/toscience-server/conf
 cp $ARCHIVE_HOME/conf/heritrix-start.sh $ARCHIVE_HOME/heritrix/
-echo "Please execute the following commands manually"
+echo "Bitte führen Sie die folgenden Befehle manuell aus:"
 echo "sudo cp $ARCHIVE_HOME/conf/elasticsearch.yml $ELASTICSEARCH_CONF"
-echo "sudo cp $ARCHIVE_HOME/conf/regal-api /etc/init.d"
-echo "sudo update-rc.d regal-api defaults 99 20"
+echo "sudo cp $ARCHIVE_HOME/conf/toscience-api /etc/init.d"
+echo "sudo chkconfig --set toscience-api on"
 echo "sudo cp $ARCHIVE_HOME/conf/tomcat6 /etc/init.d"
-echo "sudo update-rc.d tomcat6 defaults 98 15"
+echo "sudo chkconfig --set tomcat6 on"
 }
 
 copyConfig
