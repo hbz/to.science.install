1813f72b0bc508eb57aba24bc7ed0dbac49edd4e
Ingolf Kuss <kuss@hbz-nrw.de>
2023-06-22 12:55:18 +0200

    Update Doku regal-install. Monit startet regal-server von selber an.

--- install-regal-api.sh
+++ install-regal-api.sh
@@ -21,7 +21,8 @@ cd -
 cd $ARCHIVE_HOME
 
 OLDDIR=`readlink regal-server`
-OLDPORT=`grep "http.port" $OLDDIR/conf/application.conf | grep -o "[0-9]*"`
+CONF=`readlink $OLDDIR/conf`
+OLDPORT=`grep "http.port" $CONF/application.conf | grep -o "[0-9]*"`
 
 if [ $OLDPORT -eq 9000 ]
 then
@@ -35,22 +36,26 @@ unzip regal-api/target/universal/regal-api-*zip -d tmp >/dev/null
 regalServerDir=regal-server.`date  +"%Y%m%d%H%M"`
 mv tmp/regal-api* $regalServerDir
 rm -rf tmp
-sed -e "s/^http\.port=.*$/http\.port=$PLAYPORT/" regal-server/conf/application.conf > $regalServerDir/conf/application.conf
-cp regal-server/conf/mail.properties $regalServerDir/conf/
+sed -e "s/^http\.port=.*$/http\.port=$PLAYPORT/" $CONF/application.conf > /tmp/application.conf
+mv /tmp/application.conf $CONF/application.conf
+rm -r $regalServerDir/conf
+ln -s $CONF $regalServerDir/conf
 rm $ARCHIVE_HOME/regal-server
 ln -s $regalServerDir $ARCHIVE_HOME/regal-server
 
 echo ""
 echo "New executable available under $regalServerDir." 
-echo "Provide port info at $regalServerDir/conf/application.conf." 
-echo "Current port is set to $PLAYPORT"
-echo "Ready to switch from $OLDDIR to $NEWDIR. From old port $OLDPORT to new $PLAYPORT"
+echo "Provided port info at $CONF/application.conf." 
+echo "Ready to switch from $OLDDIR to $regalServerDir. From old port $OLDPORT to new $PLAYPORT"
+echo "monit has automatically started the new executable under port $PLAYPORT. Check this with \"ps -eaf | grep regal-server\""
+echo "(if you do not want monit to start the new executable automatically, you should have done \"monit unmonitor regal-api\" before you started this script)"
+echo "If the new executable should not yet be running, try to start it with \"sudo service regal-api start\""
 echo ""
-echo "Please update port information in your apache conf to $PLAYPORT!" 
-echo "cp ../prod_conf/site.ssl.conf ../prod_conf/site.ssl.conf.bck ; cat ../prod_conf/site.ssl.conf|sed s/"$OLDPORT"/"$PLAYPORT"/g >tmp; mv tmp ../prod_conf/site.ssl.conf"
-echo "To perform switch, execute:"  
-# echo "sudo service regal-api start" # monit startet den neuen Dienst schon automatisch
+echo "Refresh Cache:"
 echo "./loadCache.sh"
+echo ""
+echo "To perform switch, update the port information in your apache conf to $PLAYPORT!" 
+echo "cp ../prod_conf/site.ssl.conf ../prod_conf/site.ssl.conf.bck ; cat ../prod_conf/site.ssl.conf|sed s/"$OLDPORT"/"$PLAYPORT"/g >tmp; mv tmp ../prod_conf/site.ssl.conf"
 echo "sudo service apache2 reload"
 echo "kill `cat $OLDDIR/RUNNING_PID`"
 
