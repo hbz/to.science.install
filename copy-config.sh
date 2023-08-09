e66469edca898e831253b1b38943e9e02038c04e
Ingolf kuss <kuss@hbz-nrw.de>
2023-08-09 18:53:35 +0200

    Umbenennung Verz. api => api.conf   - auch Typo : toscience-api.server => toscience-api.service (!)

--- copy-config.sh
+++ copy-config.sh
@@ -8,11 +8,11 @@ cp $ARCHIVE_HOME/conf/tomcat-users.xml $TOMCAT_CONF
 cp $ARCHIVE_HOME/conf/fedora-users.xml $ARCHIVE_HOME/fedora/server/config/
 cp $ARCHIVE_HOME/conf/fedora.fcfg $ARCHIVE_HOME/fedora/server/config/
 cp $ARCHIVE_HOME/conf/setenv.sh $TOMCAT_HOME/bin
-cp $ARCHIVE_HOME/conf/application.conf /etc/toscience/api
+cp $ARCHIVE_HOME/conf/application.conf /etc/toscience/api.conf
 cp $ARCHIVE_HOME/conf/heritrix-start.sh $ARCHIVE_HOME/heritrix/
 echo "Bitte führen Sie die folgenden Befehle manuell aus:"
 echo "sudo cp $ARCHIVE_HOME/conf/elasticsearch.yml $ELASTICSEARCH_CONF"
-echo "sudo cp $ARCHIVE_HOME/conf/toscience-api.server /etc/systemd/system"
+echo "sudo cp $ARCHIVE_HOME/conf/toscience-api.service /etc/systemd/system"
 echo "sudo chkconfig --set toscience-api on"
 echo "sudo cp $ARCHIVE_HOME/conf/tomcat6 /etc/init.d"
 echo "sudo chkconfig --set tomcat6 on"
