d773fb4fab925ed186b0c0d3511b40ce1e822d48
Ingolf Kuss <kuss@hbz-nrw.de>
2024-08-16 13:06:12 +0200

    Add fail2ban Service

--- restart-all.sh
+++ restart-all.sh
@@ -9,6 +9,7 @@ sudo systemctl restart toscience-labels.service
 sudo systemctl restart toscience-forms.service
 sudo systemctl restart skos-lookup.service
 sudo systemctl restart toscience-api.service
+sudo systemctl restart fail2ban.service
 sudo systemctl restart apache2.service
-sudo systemctl restart fph56-fpm.service
+sudo systemctl restart php56-fpm.service
 
